57d1a9152bdfe951af9bcfc1cdf8786b6f2ef51e
Aletyg <127630275+Aletyg@users.noreply.github.com>
2026-04-18 11:23:52 +0200

    Add files via upload

---
+++ api/news.js
@@ -0,0 +1,7 @@
+// api/news.js
+export default async function handler(req, res) {
+  const API_KEY = process.env.NEWS_API_KEY;
+  const response = await fetch(`https://newsapi.org/v2/everything?q=boxing&apiKey=${API_KEY}`);
+  const data = await response.json();
+  res.status(200).json(data);
+}
